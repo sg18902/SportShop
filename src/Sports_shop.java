3557cacd6f9ae2b12378b7e318ac9bbc28a82554
Shivam Goswami <107423842+sg18902@users.noreply.github.com>
2023-10-24 11:33:10 +0530

    first commit

---
+++ src/Sports_shop.java
@@ -0,0 +1,22 @@
+ /*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+/**
+ *
+ * @author shivam
+ */
+public class Sports_shop {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new initro().setVisible(true);
+        // TODO code application logic here
+    }
+    
+}
